e8eeca5c3e43caf89014f804a2c39e77208f9709
wosker <tyy840913@qq.com>
2025-04-05 09:26:56 +0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,8 +6,8 @@ const UUID = process.env.UUID || '0865b73d-6b5d-42f3-aac5-68a026553d8d'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || ';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '443';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '';   // 没用哪吒，把这个参数空着
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.woskee.dpdns.org';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMDhjOWUxMWEwZWM0NThlNWJmMWQ4N2Y5N2E3NjJlZTciLCJ0IjoiNmNmNDdmMDktM2UzZi00MDMyLWEyZmUtM2ExNGQzY2RiYzJkIiwicyI6Ik16Um1OREkyWWpBdFpqSXhOeTAwTkRBd0xUaGhaamN0T1RObE0ySTNaVFF4WXpRNSJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'test.woskee.dpdns.org';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiMDhjOWUxMWEwZWM0NThlNWJmMWQ4N2Y5N2E3NjJlZTciLCJ0IjoiZjBkYzc1MjUtMDRlNy00NzkwLWFjNDgtM2IxMWNhOWRiOWQ0IiwicyI6IllqZ3laV0ZtWm1NdE9UaGxZeTAwWmpFeExXRmtOall0WmprMVptRm1abVU0T1dWbSJ9';
 const CFIP = process.env.CFIP || 'www.visa.com.tw';
 const NAME = process.env.NAME || 'Choreo';
 
